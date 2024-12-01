b8d6fc4c05374e823308d1b34a558eb9a28eb21d
Michael Limiero <mike5713@gmail.com>
2024-12-01 17:35:19 -0500

    day 1

---
+++ src/bin/day01.rs
@@ -0,0 +1,70 @@
+use std::{io::{Read, stdin}, collections::HashMap};
+
+#[cfg(test)]
+static TEST_INPUT: &str = "3   4
+4   3
+2   5
+1   3
+3   9
+3   3";
+
+fn parse(input: &str) -> (Vec<i32>, Vec<i32>) {
+    let mut list1 = Vec::<i32>::new();
+    let mut list2 = Vec::<i32>::new();
+
+    for line in input.lines() {
+        let words: Vec<&str> = line.split_whitespace().collect();
+        let i1: i32 = words[0].parse().expect("not an integer");
+        let i2: i32 = words[1].parse().expect("not an integer");
+        list1.push(i1);
+        list2.push(i2);
+    }
+
+    (list1, list2)
+}
+
+fn part1(input: &str) -> i32 {
+    let (mut list1, mut list2) = parse(input);
+    list1.sort();
+    list2.sort();
+
+    let mut ans = 0;
+    for i in 0..list1.len() {
+        ans += (list1[i] - list2[i]).abs();
+    }
+
+    ans
+}
+
+#[test]
+fn test_part1() {
+    assert_eq!(part1(TEST_INPUT), 11);
+}
+
+fn part2(input: &str) -> i32 {
+    let (list1, list2) = parse(input);
+    let mut occurrences = HashMap::<i32,i32>::new();
+    let mut ans = 0;
+
+    for x in list2 {
+        occurrences.entry(x).and_modify(|n| *n += 1).or_insert(1);
+    }
+
+    for x in list1 {
+        ans += x * occurrences.get(&x).unwrap_or(&0);
+    }
+
+    ans
+}
+
+#[test]
+fn test_part2() {
+    assert_eq!(part2(TEST_INPUT), 31);
+}
+
+fn main() {
+    let mut input = String::new();
+    stdin().read_to_string(&mut input).unwrap();
+    println!("Part 1: {}", part1(&input));
+    println!("Part 2: {}", part2(&input));
+}
